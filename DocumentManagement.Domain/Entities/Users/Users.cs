ed22e5382a944e26ec50ff2e1a3a19337ddf9a4e
Tran van Phong <tranvanphong.owner@gmail.com>
2024-07-08 10:13:21 +0700

    Add users and roles entities

---
+++ DocumentManagement.Domain/Entities/Users/Users.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DocumentManagement.Domain.Entities.Users
+{
+    internal class Users
+    {
+
+        [Key] 
+        public int Users_id { get; set; }
+
+        [Required] 
+        [StringLength(50)] 
+        public string First_name { get; set; }
+
+        [Required]
+        [StringLength(50)]
+        public string Last_name { get; set; }
+
+        [StringLength(100)]
+        public string Address { get; set; }
+
+        [Required]
+        [EmailAddress] 
+        public string Email { get; set; }
+
+        [Required]
+        [StringLength(10)]
+        public string Gender { get; set; }
+
+        [Required]
+        public string Password_hash { get; set; }
+
+        [Required]
+        [ForeignKey("Role")]
+        public int Role_id { get; set; }
+
+        public  Roles roles { get; set; }
+    }
+}
