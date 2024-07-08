4780a4175e73c5ae342724337024fb4b28cdfe6b
thanh0430 <115385832+thanh0430@users.noreply.github.com>
2024-07-08 11:03:01 +0700

    update

---
+++ DocumentManagement.Domain/Entities/Files.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DocumentManagement.Domain.Entities
+{
+    [Table("Files")]
+    internal class Files
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+        [ForeignKey("Foleders_id")]
+        public int Foleders_id { get; set; }
+        public string File_name { get; set; }
+        public string File_path { get; set; }
+        public DateTime Created_date { get; set; }
+        [ForeignKey("User_id")]
+        public int User_id { get; set; }
+        public float File_size { get; set; }
+
+        public virtual Foleders? Foleders { get; set; }
+    }
+}
---
+++ DocumentManagement.Domain/Entities/Foleders.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DocumentManagement.Domain.Entities
+{
+    [Table("Foleders")]
+    internal class Foleders
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+        public string Foleders_name { get; set; }
+        public DateTime Created_date { get; set; }
+        [ForeignKey("User_id")]
+        public int User_id { get; set; }
+        public string Folders_lever { get; set; }
+        public virtual ICollection<Files>? File { get; set; }
+        //public virtual ICollection<Users>? Users { get; set; }
+    }
+}
