c7beadb03865fb9eb8b2b950d49d009bf12e7374
thanh0430 <115385832+thanh0430@users.noreply.github.com>
2024-07-09 09:59:20 +0700

    Folder

---
+++ DocumentManagement.Application/Interfaces/IFolderService.cs
@@ -0,0 +1,18 @@
+﻿using DocumentManagement.Application.DTOs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DocumentManagement.Application.Interfaces
+{
+    public interface IFolderService
+    {
+        Task<List<Folder_DTOs>> GetAllAsync();
+        Task<Folder_DTOs> GetByIdAsync(int Id);
+        Task<Folder_DTOs> AddAsync(Folder_DTOs Folder);
+        Task UpdateAsync(Folder_DTOs Folder, int Id);
+        Task DeleteAsync(int Id);
+    }
+}
---
+++ DocumentManagement.Domain/Migrations/20240709023159_vt1.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DocumentManagement.Domain.Migrations
+{
+    /// <inheritdoc />
+    public partial class vt1 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
