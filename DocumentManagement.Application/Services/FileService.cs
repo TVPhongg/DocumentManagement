ec95a709f3b8da54d7d5aa8e2df08ef44591217f
thanh0430 <115385832+thanh0430@users.noreply.github.com>
2024-07-15 14:12:12 +0700

    Feature file

--- DocumentManagement.Application/Services/FileService.cs
+++ DocumentManagement.Application/Services/FileService.cs
@@ -1,4 +1,8 @@
-﻿using System;
+﻿using DocumentManagement.Application.DTOs;
+using DocumentManagement.Application.Interfaces;
+using DocumentManagement.Domain.Context;
+using Microsoft.EntityFrameworkCore;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +10,37 @@ using System.Threading.Tasks;
 
 namespace DocumentManagement.Application.Services
 {
-    internal class FileService
+    public class FileService : IFileService
     {
+        private readonly MyDbContext _dbContext;
+
+        public FileService(MyDbContext dbContext) 
+        {
+            _dbContext = dbContext;
+        }
+        public Task AddFile(File_DTOs File)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task DeleteFile(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<File_DTOs>> GetAllFile(int user_id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<File_DTOs>> SearchFile(string searchTerm)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task UpdateFile(File_DTOs File, int id)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
